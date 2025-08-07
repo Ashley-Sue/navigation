e941180e98b2dae2f949f183d5d421498f7af08d
Ashley Sue <ashley.sue0605@gmail.com>
2025-08-07 10:21:22 -0700

    Added NavigationStack with a NavigatiobnLink to second view

--- navigation/ContentView.swift
+++ navigation/ContentView.swift
@@ -9,15 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        NavigationStack {
+            VStack{
+                Text("This is the root view 🌳")
+                NavigationLink(destination: Text("You've arrived at the Second View!")) {
+                    Text("Click me!")
+                }
+            }//end VStack
+        }//end NavigationStack
+    }//end body
+}//end struct
 
 #Preview {
     ContentView()
